9a95b51bb8a4757211f7b68a3aa5bba3cef5d2d3
John Neo Lopez <133441327+Lxndrmrdjss@users.noreply.github.com>
2025-04-16 01:44:16 -0400

    server testing

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dbCredentials: { url: process.env.DATABASE_URL="file:local.db" },
 	verbose: true,
 	strict: true,
 	dialect: 'turso'
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,5 +2,7 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
 	id: integer('id').primaryKey(),
-	age: integer('age')
+	name: text('name').notNull(),
+	age: integer('age').notNull()
 });
+
